d1b1d9385cf43018c839ac681b858a567110901f
rodneyg@users.noreply.github.com <Rodney@Rodneys-MacBook-Pro.local>
2016-12-21 02:24:28 -0800

    added animations and commented code

--- HustleCo/HomeViewController.swift
+++ HustleCo/HomeViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet var bgImage: UIImageView!
     @IBOutlet var headline: UILabel!
     @IBOutlet var writer: UIButton!
     @IBOutlet var messageButton: UIButton!
     @IBOutlet var readButton: UIButton!
+    @IBOutlet var writerImage: UIImageView!
     
     var currentArticle: Article!
     var articles: ArticleQueue<Article>!
@@ -21,6 +23,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addTapGesture()
         loadArticles()
     }
     
@@ -31,20 +34,39 @@ class HomeViewController: UIViewController {
     }
     
     func doubleTapped() {
-        
+        next()
     }
     
+    //goes to next article in the queue, if empty loads more articles
     func next() {
         guard let next = articles.peek() else {
             loadArticles()
             return
         }
         
+        currentArticle = next
+        animate()
         
+        //change text
+        headline.text = currentArticle.headline
+        writer.setTitle(currentArticle.writer, for: .normal)
     }
     
+    //animates the background and writer image
     func animate() {
+        guard let bgImg = UIImage(named: currentArticle.imageName), let writerImg = UIImage(named: currentArticle.writerImage) else {
+            print("Could not load images")
+            return
+        }
         
+        //fade animation on images
+        UIView.transition(with: bgImage, duration: 0.85, options: .curveEaseInOut, animations: {
+            self.bgImage.image = bgImg
+            }, completion: nil)
+        
+        UIView.transition(with: writerImage, duration: 0.85, options: .curveEaseInOut, animations: {
+            self.writerImage.image = writerImg
+            }, completion: nil)
     }
     
     @IBAction func messageTouched(_ sender: AnyObject) {
@@ -55,15 +77,17 @@ class HomeViewController: UIViewController {
         performSegue(withIdentifier: "", sender: self)
     }
     
+    //loads articles and parses them
     func loadArticles() {
         guard let json = loadJson("Articles.json") else { return }
         
         parseArticles(json: json)
     }
     
+    //parse articles from json dictionary and add to queue
     func parseArticles(json: NSDictionary) {
         guard let articlesJson : NSArray = json["articles"] as? NSArray else {
-            print("Error!! Unable to parse articles")
+            print("Error! Unable to parse articles")
             return
         }
         
@@ -74,11 +98,11 @@ class HomeViewController: UIViewController {
         }
     }
     
+    //load json from file
     func loadJson(_ fileName: String) -> NSDictionary? {
-        guard let url = Bundle.main.url(forResource: fileName, withExtension: "json") else { return nil }
-        
-        guard let data = NSData(contentsOf: url) else {
-            print("Error!! Unable to load  \(fileName).json")
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: "json"),
+        let data = NSData(contentsOf: url) else {
+            print("Error! Unable to load \(fileName).json")
             return nil
         }
         
@@ -87,10 +111,9 @@ class HomeViewController: UIViewController {
             
             return dictionary
         } catch {
-            print("Error!! Unable to parse  \(fileName).json")
+            print("Error! Unable to parse \(fileName).json")
+            return nil
         }
-        
-        return nil
     }
 }
 
